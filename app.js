5f0abcecfe55ccbd9d107c75b93e1c7352da0a8e
tony-ay <toay789@gmail.com>
2020-05-01 22:56:17 -0500

    Added basic html page and path change

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var express = require("express");
+var express = require('express');
 var app = express();
+var path = require('path');
+
+var PORT = process.env.PORT || 5000;
 
 //return nth fibonacci number
 function fibo(n) {
@@ -12,18 +15,22 @@ function fibo(n) {
     }
 }
 
+//basic root page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 //handling get requests for fib url path
-app.get("/fib", (req, res, next) => {
+app.get('/fib', (req, res) => {
     try {
         res.json(fibo(parseInt(req.query.n)));
-    }
-    catch(e) {
-        res.json("Please enter valid integer");
+    } catch (e) {
+        res.json('Please enter valid integer');
         throw new Error(e);
     }
 });
 
 //express app listening
-app.listen(4000, () => {
-    console.log("Your node js server is running");
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
 });
